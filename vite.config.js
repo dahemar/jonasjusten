c8cba3e67b0183fb7c8d9eb9b78e0384b1856bf8
dahemar <davidherreromarti@gmail.com>
2025-08-05 22:51:31 +0200

    Fix vite.config.js base path for custom domain - remove /jonasjusten/ prefix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jonasjusten/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
